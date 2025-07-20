22d39709861d7dc374df1d76347bda7ef6e6f403
mohit <mohitthakur9901@gmail.com>
2025-07-20 17:01:02 +0530

    fixed cron issue

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -1,8 +1,8 @@
-import cron from "cron";
+import { CronJob } from "cron";
 import https from "https";
 import { ENV } from "./env";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new CronJob("*/14 * * * *", function () {
   https
     .get(ENV.API_URL, (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully");
